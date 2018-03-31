227c894af21759fcc7c903b6bb2bb76f1f1749bc
Ray Tung <code@raytung.net>
2018-03-31 14:01:33 -0400

    Added basic test for help

--- e2e_test.go
+++ e2e_test.go
@@ -3,12 +3,31 @@ package main
 import (
 	"testing"
 	"os/exec"
+	"strings"
 )
 
-func TestGenerate(t *testing.T) {
-	testBinary := "g6_test"
-	exec.Command("go", "build", "-o", testBinary).Output()
-	defer func() {
-		exec.Command("rm", testBinary).Run()
-	}()
+const testBinary = "g6_test"
+
+func TestG6(t *testing.T) {
+	defer tearDown()
+	if err := exec.Command("go", "build", "-o", testBinary).Run(); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	t.Run("<no command>", func(t *testing.T) {
+		output, err := exec.Command("./" + testBinary).Output()
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
+
+		expectOutToContain := "g6 is a user friendly database migration tool"
+		if !strings.Contains(string(output), expectOutToContain) {
+			t.Fatalf("<no command> did not output string: %v", expectOutToContain)
+		}
+	})
+
+}
+
+func tearDown() {
+	exec.Command("rm", testBinary).Run()
 }
